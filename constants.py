67775f393025e49e83cfee8d68cb0dc2ea2cadef
specnazm <laketic.milena98@gmail.com>
2021-02-11 16:59:15 +0100

    Parsing alignment files

---
+++ constants.py
@@ -0,0 +1,5 @@
+VIDEO_DIRECTORY_PATH = '.\data\\videos'
+
+ALIGN_DIRECTORY_PATH = '.\data\\align'
+
+FEATURE_DIRECTORY_PATH = '.\data\\features'
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import cv2
 import numpy as np
 from glob import glob
@@ -13,19 +11,49 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from vocabulary import Vocabulary
 from util import curses_init, curses_clean_up, progress_msg
+from constants import FEATURE_DIRECTORY_PATH, VIDEO_DIRECTORY_PATH, ALIGN_DIRECTORY_PATH
 
+def parse_alignment(file):
+    f = open(file, 'r')
+    lines = f.readlines()
 
-VIDEO_DIRECTORY_PATH = 'data/videos'
-ALIGN_DIRECTORY_PATH = 'data/align'
-FEATURE_DIRECTORY_PATH = 'data/features'
+    #segments will be list of tuples (start_frame_num, end_frame_num, word) for every word in alignment file
+    segments = []
 
-DEFAULT_TRAINING_FRACTION = 0.7
+    #line in .align file is in format:   start_frame  end_frame  word
+    for line in lines:
 
-# Number of frames to use for each word.
-NUM_FRAMES = 6
+        align_elements = line.split()
+        start_frame = align_elements[0]
+        end_frame = align_elements[1]
+        word = align_elements[2]
 
-# Number of facial features to use. Maximum is 512.
-NUM_FACIAL_FEATURES = 512
+        # Round the start frame down and end frame up.
+        start_frame_num = int(round(float(start_frame) / 1000))
+        end_frame_num = int(round(float(end_frame) / 1000))
+
+        segments.append((start_frame_num, end_frame_num, word))
+
+    last_word_end_frame = segments[-1][1]
+    firs_word_start_frame = segments[0][0]
+    num_frames = last_word_end_frame - firs_word_start_frame
+    print('num of frames per video', num_frames)
+
+    # exclude first and last (cause it's sil)
+    return segments[1:-1], num_frames
+
+def process_video(speaker, vid_name):
+
+    ''' Split a video into sets of frames corresponding to each spoken word. '''
+    video_path = os.path.join(VIDEO_DIRECTORY_PATH, speaker, vid_name)
+    align_name = vid_name.split('.')[0] + '.align'
+    align_path = os.path.join(ALIGN_DIRECTORY_PATH, speaker, align_name)
+
+    cap = cv2.VideoCapture(video_path)
+
+    #alignments will be list of tuples (start_frame_num, end_frame_num, word) for every word in alignment file
+    alignments, num_frames = parse_alignment(align_path)
+
 
 
 def preprocess(speaker):
@@ -44,9 +72,12 @@ def preprocess(speaker):
 
     # Get list of all videos to process.
     video_glob = os.path.join(VIDEO_DIRECTORY_PATH, speaker, '*.mpg')
+    print(video_glob)
     video_paths = glob(video_glob)
+    print('ovoo',video_paths)
     num_videos = len(video_paths)
     word_count = 0
+    print(num_videos)
 
     stdscr.addstr(0, 0, f'Extracting facial features for speaker {speaker}. Press q to exit.')
     stdscr.refresh()
@@ -54,17 +85,14 @@ def preprocess(speaker):
     try:
         for video_ordinal, video_path in enumerate(video_paths):
             video_name = os.path.basename(video_path)
-            progress_msg(stdscr, video_count, word_count, video_name, num_videos)
+            progress_msg(stdscr, video_ordinal, word_count, video_name, num_videos)
 
+            process_video(speaker, video_name)
+        
     except (SystemExit, KeyboardInterrupt):
-        # Expected exceptions are either the generic one raised when a user
-        # presses 'q' to exit, or a KeyboardInterrupt if the user gets
-        # impatient and presses Ctrl-C.
         pass
     finally:
         curses_clean_up()
-
-
 
 
 if __name__ == '__main__':
